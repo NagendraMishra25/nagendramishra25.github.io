5b697962d66ad30a124ccefbb9a2e0794f2e573c
NagendraMishra25 <52664314+NagendraMishra25@users.noreply.github.com>
2019-07-08 22:17:31 +0800

    date tick change

--- src/app/components/securities/securities.component.ts
+++ src/app/components/securities/securities.component.ts
@@ -8,40 +8,18 @@ import { Security } from '../../models/Security';
   styleUrls: ['./securities.component.css']
 })
 export class SecuritiesComponent implements OnInit {
-  date = Date.now();
+  currTime = Date.now();
 
   securities:Security[];
 
-  constructor() { }
+  constructor() {
+    setInterval(() => {
+      this.currTime = Date.now();
+    }, 3000);
+  }
 
   ngOnInit() {
-    this.securities = [
-      {
-        "accumulated_turnover": 9070951,
-        "account_executive": 81753,
-        "daily_turnover": 9068239
-      },
-      {
-        "accumulated_turnover": 9098745,
-        "account_executive": 15410,
-        "daily_turnover": 9091744
-      },
-      {
-        "accumulated_turnover": 9137053,
-        "account_executive": 89794,
-        "daily_turnover": 9131265
-      },
-      {
-        "accumulated_turnover": 9113916,
-        "account_executive": 17347,
-        "daily_turnover": 9108161
-      },
-      {
-        "accumulated_turnover": 9192240,
-        "account_executive": 38088,
-        "daily_turnover": 9186852
-      }
-    ]
+
   }
 
 }
